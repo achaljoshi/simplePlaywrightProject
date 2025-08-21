b3d42ee14e69e107371226e8f48cf8807df122b7
achaljoshi <ajoshi@fieldca.com>
2025-08-21 19:12:18 +0530

    Set Browser to headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,15 +16,15 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: false },
+      use: { ...devices['Desktop Chrome'], headless: true },
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], headless: false },
+      use: { ...devices['Desktop Firefox'], headless: true },
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], headless: false },
+      use: { ...devices['Desktop Safari'], headless: true },
     },
   ],
 });
